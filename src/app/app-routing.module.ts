fa6ec2833d290e45ef47f5594259d0454c4a43b4
Mete Karagöz <mete.karagoz@etiya.com>
2023-10-12 14:51:04 +0300

    created feature

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CarComponent } from './pages/car/car.component';
 import { RentComponent } from './pages/rent/rent.component';
-import { CarProcessComponent } from './pages/car-process/car-process.component';
+import { CarProcessComponent } from './features/car-process/car-process.component';
 
 const routes: Routes = [
   { path: '', pathMatch: 'full', component: CarComponent },
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,21 +10,13 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import { RouterModule } from '@angular/router';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { BrandComponent } from './pages/brand/brand.component';
-import { AddBrandComponent } from './pages/car-process/add-brand/add-brand.component';
-import { AddModelComponent } from './pages/car-process/add-model/add-model.component';
-import { CarAddComponent } from './pages/car-process/add-car/car-add.component';
-import { CarProcessComponent } from './pages/car-process/car-process.component';
-import { DeleteBrandComponent } from './pages/car-process/delete-brand/delete-brand.component';
-import { DeleteCarComponent } from './pages/car-process/delete-car/delete-car.component';
-import { UpdateBrandComponent } from './pages/car-process/update-brand/update-brand.component';
-import { UpdateCarComponent } from './pages/car-process/update-car/update-car.component';
 
 import { ModelComponent } from './pages/model/model.component';
 import { RentComponent } from './pages/rent/rent.component';
 import { CarComponent } from './pages/car/car.component';
 import { SharedModule } from './shared/shared.module';
 import { CarModule } from './pages/car/car.module';
-import { CarProcessModule } from './pages/car-process/car-process.module';
+import { CarProcessModule } from './features/car-process/car-process.module';
 
 @NgModule({
   declarations: [
